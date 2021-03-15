1fc7f89290eb50d51b6d1f8758c89effc2266dfd
Brookann <blaire08@ymail.com>
2021-03-15 18:02:25 -0500

    First question done

--- EmployeeSQL/Homework.sql
+++ EmployeeSQL/Homework.sql
@@ -1,55 +1,31 @@
-CREATE TABLE "departments" (
-    "dept_no" varchar(30)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar(30)   NOT NULL
-);
-CREATE TABLE "dept_manager" (
-    "dept_no" varchar(30)   NOT NULL,
-    "emp_no" int   NOT NULL
-);
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" varchar(30)   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(30)   NOT NULL,
-    "last_name" varchar(30)   NOT NULL,
-    "sex" varchar(10)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
-);
-CREATE TABLE "titles" (
-    "title_id" varchar(30)   NOT NULL,
-    "title" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+select * from departments; 
+select * from dept_emp;
+select * from dept_manager;
+select * from employees;
+select * from salaries;
+select * from titles;
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary from employees
+	inner join salaries on employees.emp_no = salaries.emp_no order by emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
 
 
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
