e2f8e0c65aba35d91de6003de68b8705e6bf6bf1
Brookann <blaire08@ymail.com>
2021-03-15 20:03:53 -0500

    Final question done

--- EmployeeSQL/Homework.sql
+++ EmployeeSQL/Homework.sql
@@ -40,4 +40,5 @@ where dept_name = 'Sales' or dept_name = 'Department' order by emp_no;
 
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
+select last_name, count (last_name) as "frequency" from employees
+group by last_name order by count (last_name) desc;
