61e25c8aeaefc5c77ac8cd0e972e60a96d892028
Brookann <blaire08@ymail.com>
2021-03-15 19:58:27 -0500

    Seventh question done

--- EmployeeSQL/Homework.sql
+++ EmployeeSQL/Homework.sql
@@ -27,9 +27,16 @@ select first_name, last_name, sex from employees
 where first_name = 'Hercules' and last_name like 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
+	inner join dept_emp de on de.emp_no = e.emp_no
+	inner join departments d on d.dept_no = de.dept_no
+where dept_name = 'Sales' order by emp_no;
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
+	inner join dept_emp de on de.emp_no = e.emp_no
+	inner join departments d on d.dept_no = de.dept_no
+where dept_name = 'Sales' or dept_name = 'Department' order by emp_no;
 
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
